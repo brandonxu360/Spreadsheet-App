601cdc27a31ab941ad6bf988776137c57b5ccf22
bxu <brandon.xu@wsu.edu>
2024-01-20 23:21:17 -0800

    Implement getUserNums function to get/store input nums

--- HW1/Program.cs
+++ HW1/Program.cs
@@ -1,9 +1,23 @@
-﻿namespace HW1
+﻿using System;
+
+namespace HW1
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            var inputNums = getUserNums();
+        }
+        
+        // Prompts user for a line of numbers seperated by spaces.
+        // Returns the array of numbers as substrings
+        // *Assumes input is correctly formatted
+        private static string[] getUserNums()
+        {
+            Console.WriteLine("Enter a collection of numbers in the range [0, 100], seperated by spaces: ");
+            var input = Console.ReadLine();
+            return input.Split(' ');
         }
     }
 }
+
