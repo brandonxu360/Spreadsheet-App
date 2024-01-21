8e236525c2d4d76100cfe5905ebd9c414a90930f
bxu <brandon.xu@wsu.edu>
2024-01-21 03:23:05 -0800

    Implement recursive DisplayInOrder function for BST

--- HW1/BST.cs
+++ HW1/BST.cs
@@ -21,5 +21,27 @@ namespace HW1
                 Root.Add(value);
             }
         }
+        
+        // Public function to display BST node values in order
+        public void DisplayInOrder()
+        {
+            Console.Write('[');
+            DisplayInOrder(Root); // Call private recursive in-order function on root
+            Console.Write(']');
+        }
+
+        // Private recursive function to traverse the BST in order and write the values of the nodes to console
+        // Used in public DisplayInOrder function
+        private void DisplayInOrder(BSTNode node) 
+        {
+            // If left subtree exists, traverse left
+            if (node.LNode != null) DisplayInOrder(node.LNode);
+            
+            // Write current node value to console
+            Console.Write($" {node.Value} ");
+            
+            // If right subtree exists, traverse right
+            if (node.RNode != null) DisplayInOrder(node.RNode);
+        }
     }
 }
