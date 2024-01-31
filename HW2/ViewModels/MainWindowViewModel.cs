6ac642093866f71d32c047e5cce64deba22282a8
bxu <brandon.xu@wsu.edu>
2024-01-31 02:25:40 -0800

    Set up project

---
+++ HW2/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace HW2.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    public MainWindowViewModel()
+    {
+        Greeting = RunDistinctIntegers();
+    }
+    private string RunDistinctIntegers() // this is your method
+    {
+        return "Not yet implemented yet";
+    }
+    public string Greeting { get; set;}
+}
