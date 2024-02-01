9ef45ba19c70e728a758396d725d39df2ef0896c
bxu <brandon.xu@wsu.edu>
2024-02-01 15:34:53 -0800

    Add additional documentation

--- HW2/ViewModels/MainWindowViewModel.cs
+++ HW2/ViewModels/MainWindowViewModel.cs
@@ -25,6 +25,12 @@ public class MainWindowViewModel : ViewModelBase
     /// </summary>
     public string Greeting { get; set; }
 
+    /// <summary>
+    /// Creates a list of 10,000 random integers in the range [0, 20000] and counts the number of
+    /// distinct integers using a hash set method, a O(1) time complexity method, and a sorted
+    /// list method.
+    /// </summary>
+    /// <returns>String that complies the text results from each method.</returns>
     private string RunDistinctIntegers()
     {
         // Instantiate an instance of the DistinctIntCounter service
@@ -33,16 +39,38 @@ public class MainWindowViewModel : ViewModelBase
         // Create the random list of 10,000 random integers in the range [0, 20,000]
         List<int> randomIntList = this.GenerateRandomIntList(10000, 0, 20000);
 
-        return "Default Return";
+        // Get results for each method of counting distinct integers
+        int hashSetCountResult = distinctIntCounter.CountWithHashSet(randomIntList);
+        int o1SpaceCountResult = distinctIntCounter.CountWithO1Space(randomIntList);
+        int sortCountResult = distinctIntCounter.CountWithSort(randomIntList);
+
+        // Compile and return the results as a string
+        string resultString = $"1. HashSet method: {hashSetCountResult} distinct integers\n";
+        resultString += $"2. O(1) Space method: {o1SpaceCountResult} distinct integers\n";
+        resultString += $"3. Sorted list method: {sortCountResult} distinct integers\n";
+
+        return resultString;
     }
 
+    /// <summary>
+    /// Generates a list of random integers in the input range (inclusive) of the input size.
+    /// </summary>
+    /// <param name="count">The size of the generated list (number of random integers).</param>
+    /// <param name="minValue">The minimum value for the randomly generated integers (inclusive).</param>
+    /// <param name="maxValue">The maximum value for the randomly generated integers (inclusive).</param>
+    /// <returns>The list of random integers.</returns>
     private List<int> GenerateRandomIntList(int count, int minValue, int maxValue)
     {
+        // Initialize the new list
         var randomList = new List<int>();
+
+        // Instantiate the random object
         var random = new Random();
 
+        // Generate the input amount of random integers
         for (var i = 0; i < count; i++)
         {
+            // Add a random integer from the input range (inclusive)
             randomList.Add(random.Next(minValue, maxValue + 1));
         }
 
