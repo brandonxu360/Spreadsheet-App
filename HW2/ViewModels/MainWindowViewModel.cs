2c39eb5ba3f9837e1ddf7e853d08a0ed33d3d2f8
bxu <brandon.xu@wsu.edu>
2024-02-01 03:57:52 -0800

    Implement GenerateRandomIntsList helper function in MainWindowViewModel

--- HW2/ViewModels/MainWindowViewModel.cs
+++ HW2/ViewModels/MainWindowViewModel.cs
@@ -1,14 +1,47 @@
 ﻿namespace HW2.ViewModels;
 
+using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// MainWindowViewModel.
+/// </summary>
 public class MainWindowViewModel : ViewModelBase
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MainWindowViewModel"/> class.
+    /// </summary>
     public MainWindowViewModel()
     {
-        Greeting = RunDistinctIntegers();
+        this.Greeting = this.RunDistinctIntegers();
     }
-    private string RunDistinctIntegers() // this is your method
+
+    /// <summary>
+    /// Gets or sets the Greeting property.
+    /// </summary>
+    public string Greeting { get; set; }
+
+    private string RunDistinctIntegers()
     {
-        return "Not yet implemented yet";
+        // Instantiate an instance of the DistinctIntCounter service
+        DistinctIntCounter distinctIntCounter = new DistinctIntCounter();
+
+        // Create the random list of 10,000 random integers in the range [0, 20,000]
+        List<int> randomIntList = this.GenerateRandomIntsList(10000, 0, 20000);
+
+        return "Default Return";
     }
-    public string Greeting { get; set;}
+
+    private List<int> GenerateRandomIntsList(int count, int minValue, int maxValue)
+    {
+        var randomList = new List<int>();
+        var random = new Random();
+
+        for (var i = 0; i < count; i++)
+        {
+            randomList.Add(random.Next(minValue, maxValue + 1));
+        }
+
+        return randomList;
+    }
 }
