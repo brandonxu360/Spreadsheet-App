21d140d492fee99b0924cdc1828c97aa83baaeb7
bxu <brandon.xu@wsu.edu>
2024-02-01 15:15:49 -0800

    TDD Write unit tests for CountWithSort() function

--- HW2Tests/DistinctIntCounterTests.cs
+++ HW2Tests/DistinctIntCounterTests.cs
@@ -19,7 +19,7 @@ public class DistinctIntCounterTests
     {
     }
 
-    // COUNT WITH HASH SET TESTS
+    // COUNT WITH HASH SET TESTS METHODS
 
     /// <summary>
     /// Unit test for an input list with five distinct integers and no duplicates.
@@ -93,7 +93,7 @@ public class DistinctIntCounterTests
         Assert.That(result, Is.EqualTo(0));
     }
 
-    // COUNT WITH O(1) SPACE
+    // COUNT WITH O(1) SPACE METHODS
 
     /// <summary>
     /// Unit test for an input list with five distinct integers and no duplicates.
@@ -164,6 +164,82 @@ public class DistinctIntCounterTests
         DistinctIntCounter distinctIntCounter = new DistinctIntCounter();
 
         int result = distinctIntCounter.CountWithO1Space(list);
+
+        // Result should be 0 distinct integers []
+        Assert.That(result, Is.EqualTo(0));
+    }
+
+    // COUNT WITH SORT METHODS
+
+    /// <summary>
+    /// Unit test for an input list with five distinct integers and no duplicates.
+    /// </summary>
+    [Test]
+    public void CountWithSort_AllDistinct()
+    {
+        // Test case input - five distinct integers
+        List<int> list = [1, 2, 3, 4, 5];
+
+        // Instantiate the distinctIntCounter service
+        DistinctIntCounter distinctIntCounter = new DistinctIntCounter();
+
+        int result = distinctIntCounter.CountWithSort(list);
+
+        Console.WriteLine(result);
+
+        // Result should be 5 distinct integers [1, 2, 3, 4, 5]
+        Assert.That(result, Is.EqualTo(5));
+    }
+
+    /// <summary>
+    /// Unit test for a input list with 5 duplicate integers.
+    /// </summary>
+    [Test]
+    public void CountWithSort_AllDuplicate()
+    {
+        // Test case input - input list with 5 duplicate integers
+        List<int> list = [4, 4, 4, 4, 4];
+
+        // Instantiate the distinctIntCounter service
+        DistinctIntCounter distinctIntCounter = new DistinctIntCounter();
+
+        int result = distinctIntCounter.CountWithSort(list);
+
+        // Result should be 1 distinct integer [4]
+        Assert.That(result, Is.EqualTo(1));
+    }
+
+    /// <summary>
+    /// Unit test for an input list of duplicates and distinct integers (mixed).
+    /// </summary>
+    [Test]
+    public void CountWithSort_DistinctAndDuplicate()
+    {
+        // Test case input - empty list
+        List<int> list = [1, 5, 3, 3, 5, 7, 6, 4, 4];
+
+        // Instantiate the distinctIntCounter service
+        DistinctIntCounter distinctIntCounter = new DistinctIntCounter();
+
+        int result = distinctIntCounter.CountWithSort(list);
+
+        // Result should be 6 distinct integers [1, 3, 4, 5, 6, 7]
+        Assert.That(result, Is.EqualTo(6));
+    }
+
+    /// <summary>
+    /// Unit test for an empty input list.
+    /// </summary>
+    [Test]
+    public void CountWithSort_EmptyList()
+    {
+        // Test case input - empty list
+        List<int> list = [];
+
+        // Instantiate the distinctIntCounter service
+        DistinctIntCounter distinctIntCounter = new DistinctIntCounter();
+
+        int result = distinctIntCounter.CountWithSort(list);
 
         // Result should be 0 distinct integers []
         Assert.That(result, Is.EqualTo(0));
