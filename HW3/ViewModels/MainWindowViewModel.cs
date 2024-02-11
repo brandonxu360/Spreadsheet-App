ac42de998b5fe0daa8686e6d5910c2b17c768be2
bxu <brandon.xu@wsu.edu>
2024-02-10 19:12:54 -0800

    Add and configure starter code
    - Paste starter code
    - Add missing _fibonacciNumbers field and bind to TextBox in MainWindow
    - Implement missing LoadText function
    - Create top-docked Menu and link LoadFromFile to MenuItem

---
+++ HW3/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,70 @@
+﻿// <copyright file="MainWindow.axaml.cs" company="CptS 321 Instructor">
+// Copyright (c) CptS 321 Instructor. All rights reserved.
+// </copyright>
+
+namespace HW3.ViewModels;
+
+using System.IO;
+using System.Reactive;
+using System.Reactive.Linq;
+using ReactiveUI;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    private string _fibonacciNumbers = "";
+
+    public MainWindowViewModel()
+    {
+
+        // Create an interaction between the view model and the view for the file to be loaded:
+        AskForFileToLoad = new Interaction<Unit, string?>();
+
+        // Similarly to load, there is a need to create an interaction for saving into a file:
+        // TODO: Your code goes here.
+    }
+
+    /// <summary>
+    /// This is a property that will notify the user interface when changed.
+    /// TODO: You need to bind this property in the .axaml file
+    /// </summary>
+    public string FibonacciNumbers
+    {
+        get => _fibonacciNumbers;
+        private set => this.RaiseAndSetIfChanged(ref _fibonacciNumbers, value);
+    }
+
+    /// <summary>
+    /// This method will be executed when the user wants to load content from a file.
+    /// </summary>
+    public async void LoadFromFile()
+    {
+        // Wait for the user to select the file to load from.
+        var filePath = await AskForFileToLoad.Handle(default);
+        if (filePath == null) return;
+
+        // If the user selected a file, create the stream reader and load the text.
+        var textReader = new StreamReader(filePath);
+        LoadText(textReader);
+        textReader.Close();
+    }
+
+    public async void SaveToFile()
+    {
+        // TODO: Implement this method.
+    }
+
+    public Interaction<Unit, string?> AskForFileToLoad { get; }
+
+    // other code...
+    
+    /// <summary>
+    /// Reads all the characters from a file using the input TextReader and replaces the existing content in the _fibonacci field
+    /// </summary>
+    /// <param name="sr">The input TextReader for the file text contents</param>
+    private void LoadText(TextReader sr)
+    {
+        // Replace
+        FibonacciNumbers = sr.ReadToEnd();
+    }
+}
+
---
+++ HW3/Views/MainWindow.axaml.cs
@@ -0,0 +1,77 @@
+// <copyright file="MainWindow.axaml.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+using System;
+using Avalonia.Platform.Storage;
+
+namespace HW3.Views;
+
+using System.Collections.Generic;
+using System.Reactive;
+using System.Threading.Tasks;
+using Avalonia.Controls;
+using Avalonia.ReactiveUI;
+using HW3.ViewModels;
+using ReactiveUI;
+
+/// <summary>
+/// This class handles all necessary UI events to communicate with the view model and sub windows.
+/// </summary>
+public partial class MainWindow : ReactiveWindow<MainWindowViewModel>
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MainWindow"/> class.
+    /// </summary>
+    public MainWindow()
+    {
+        InitializeComponent();
+        DataContext = new MainWindowViewModel();
+
+        this.WhenActivated(d => d(ViewModel!.AskForFileToLoad.RegisterHandler(DoOpenFile)));
+
+        // TODO: add code for saving
+    }
+
+    // Use the following version of DoOpenFile if you are using Avalonia 11
+    /// <summary>
+    /// Opens a dialog to select a file which will be used to load content.
+    /// </summary>
+    /// <param name="interaction">Defines the Input/Output necessary for this workflow to complete successfully.</param>
+    /// <returns>A <see cref="Task"/> representing the result of the asynchronous operation.</returns>
+    private async Task DoOpenFile(InteractionContext<Unit, string?> interaction)
+    {
+        // Get top level from the current control. Alternatively, you can use Window reference instead.
+        var topLevel = TopLevel.GetTopLevel(this);
+        Console.Write(topLevel);
+
+        // List of filtered types
+        var fileTypes = new List<FilePickerFileType> { FilePickerFileTypes.TextPlain };
+
+
+        if (topLevel != null)
+        {
+            // Start async operation to open the dialog.
+            var filePath = await topLevel.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
+            {
+                Title = "Open Text File",
+                AllowMultiple = false,
+                FileTypeFilter = fileTypes,
+            });
+
+            interaction.SetOutput(filePath.Count == 1 ? filePath[0].Path.AbsolutePath : null);
+        }
+    }
+
+    /// <summary>
+    /// Opens a dialog to select a file in which content will be saved.
+    /// </summary>
+    /// <param name="interaction">Defines the Input/Output necessary for this workflow to complete successfully.</param>
+    /// <returns>A <see cref="Task"/> representing the result of the asynchronous operation.</returns>
+    private async Task DoSaveFile(InteractionContext<Unit, string?> interaction)
+    {
+        // TODO: your code goes here.
+    }
+}
+
+
