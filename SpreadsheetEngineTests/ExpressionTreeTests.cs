80c3a022e364c57f0f491446e729902284a6ef6a
Brandon Xu <brandon.xu@wsu.edu>
2024-03-06 01:26:51 -0800

    TDD create test cases for AdditionNode, SubtractionNode, MultiplicationNode, and DivisionNode evaluate method

--- SpreadsheetEngineTests/ExpressionTreeTests.cs
+++ SpreadsheetEngineTests/ExpressionTreeTests.cs
@@ -75,4 +75,112 @@ internal class ExpressionTreeTests
         // Assert
         Assert.That(result, Is.EqualTo(0));
     }
+
+    /// <summary>
+    /// Tests the evaluate method of AdditionNode in a normal case.
+    /// </summary>
+    /// <param name="leftValue">The double value of the left child node.</param>
+    /// <param name="rightValue">The double value of the right child node.</param>
+    /// <returns>The double sum of the right and left values.</returns>
+    [Test]
+    [TestCase(5, 3, ExpectedResult = 8)] // Two positive inputs
+    [TestCase(0, 0, ExpectedResult = 0)] // Both zero inputs
+    [TestCase(-3, -4, ExpectedResult = -7)] // Two negative inputs
+    [TestCase(5.5, 2.5, ExpectedResult = 8)] // Fractional inputs
+    public double AdditionNodeEvaluateTest(double leftValue, double rightValue)
+    {
+        // Arrange
+        var additionNode = new AdditionNode
+        {
+            LeftChild = new ValueNode(leftValue),
+            RightChild = new ValueNode(rightValue),
+        };
+
+        // Act
+        var result = additionNode.Evaluate();
+
+        // Assert
+        return result;
+    }
+
+    /// <summary>
+    /// Tests the evaluate method of SubtractionNode in a normal case.
+    /// </summary>
+    /// <param name="leftValue">The double value of the left child node.</param>
+    /// <param name="rightValue">The double value of the right child node.</param>
+    /// <returns>The double difference of the left and right values.</returns>
+    [Test]
+    [TestCase(5, 3, ExpectedResult = 2)] // Two positive inputs
+    [TestCase(0, 0, ExpectedResult = 0)] // Both zero inputs
+    [TestCase(-3, -4, ExpectedResult = 1)] // Two negative inputs
+    [TestCase(5.5, 2.5, ExpectedResult = 3)] // Fractional inputs
+    public double SubtractionNodeEvaluateTest(double leftValue, double rightValue)
+    {
+        // Arrange
+        var subtractionNode = new SubtractionNode()
+        {
+            LeftChild = new ValueNode(leftValue),
+            RightChild = new ValueNode(rightValue),
+        };
+
+        // Act
+        var result = subtractionNode.Evaluate();
+
+        // Assert
+        return result;
+    }
+
+    /// <summary>
+    /// Tests the evaluate method of MultiplicationNode in a normal case.
+    /// </summary>
+    /// <param name="leftValue">The double value of the left child node.</param>
+    /// <param name="rightValue">The double value of the right child node.</param>
+    /// <returns>The double result of multiplying the left and right values.</returns>
+    [Test]
+    [TestCase(2, 3, ExpectedResult = 6)] // Two positive inputs
+    [TestCase(0, 0, ExpectedResult = 0)] // Both zero inputs
+    [TestCase(-3, -4, ExpectedResult = 12)] // Two negative inputs
+    [TestCase(0.5, 4, ExpectedResult = 2)] // Fractional inputs
+    public double MultiplicationNodeEvaluateTest(double leftValue, double rightValue)
+    {
+        // Arrange
+        var multiplicationNode = new MultiplicationNode()
+        {
+            LeftChild = new ValueNode(leftValue),
+            RightChild = new ValueNode(rightValue),
+        };
+
+        // Act
+        var result = multiplicationNode.Evaluate();
+
+        // Assert
+        return result;
+    }
+
+    /// <summary>
+    /// Tests the evaluate method of DivisionNode in a normal case.
+    /// </summary>
+    /// <param name="leftValue">The double value of the left child node.</param>
+    /// <param name="rightValue">The double value of the right child node.</param>
+    /// <returns>The double result of dividing the left value by the right value.</returns>
+    [Test]
+    [TestCase(6, 3, ExpectedResult = 2)] // Two positive inputs
+    [TestCase(0, 1, ExpectedResult = 0)] // Left input zero
+    [TestCase(-12, -4, ExpectedResult = 3)] // Two negative inputs
+    [TestCase(4, 0.5, ExpectedResult = 8)] // Fractional inputs
+    public double DivisionNodeEvaluateTest(double leftValue, double rightValue)
+    {
+        // Arrange
+        var divisionNode = new DivisionNode()
+        {
+            LeftChild = new ValueNode(leftValue),
+            RightChild = new ValueNode(rightValue),
+        };
+
+        // Act
+        var result = divisionNode.Evaluate();
+
+        // Assert
+        return result;
+    }
 }
